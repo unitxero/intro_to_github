377feae618cba041ec71f2839784599def025649
unitxero <65042091+unitxero@users.noreply.github.com>
2023-05-09 13:26:11 -0400

    added a function named print_name

---
+++ my_first_file.py
@@ -0,0 +1,7 @@
+print("Hello AIS")
+print("Thanks for coming")
+"""I want a function to print the name of the person who is coming to the party"""
+
+
+def print_name(name):
+    print("Hello {}".format(name))
